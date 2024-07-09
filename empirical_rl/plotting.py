862c70467202d8d2cfd6e523199b15b7e18fe3a4
Ju T <53004817+JulienT01@users.noreply.github.com>
2024-07-09 10:54:32 +0200

    Update plotting.py
    Use ExperimentManager instead of direct path

--- empirical_rl/plotting.py
+++ empirical_rl/plotting.py
@@ -1,18 +1,30 @@
 from rlberry.manager import plot_writer_data
+import pathlib
+from rlberry.manager.experiment_manager import ExperimentManager
+
+default_ppo = "data_training_default_ppo"
+path_to_load = next(
+    pathlib.Path(default_ppo).glob("**/manager_obj.pickle")
+)  # find the path to the "manager_obj.pickle"
+loaded_experiment_manager_default_ppo = ExperimentManager.load(path_to_load) 
 
 
-# aliases for exp results 
-default_ppo = "/data_training_default_ppo/manager_data/ppo-default_2024-07-03_11-26-14_b0045e63/agent_handlers/"
-avec_ppo = "data_training_avec_ppo/manager_data/avec-ppo_2024-07-03_11-26-14_9e4b15e4/"
+avec_ppo = "data_training_avec_ppo"
+path_to_load = next(
+    pathlib.Path(avec_ppo).glob("**/manager_obj.pickle")
+)  # find the path to the "manager_obj.pickle"
+loaded_experiment_manager_avec_ppo = ExperimentManager.load(path_to_load) 
 
-_ = plot_writer_data([default_ppo, avec_ppo],
+
+
+_ = plot_writer_data([loaded_experiment_manager_default_ppo, loaded_experiment_manager_avec_ppo],
     tag="rollout/ep_rew_mean",
     title="Training Episode Cumulative Rewards",
     show=False,
     savefig_fname="rewards"
 )
 
-_ = plot_writer_data([default_ppo, avec_ppo],
+_ = plot_writer_data([loaded_experiment_manager_default_ppo, loaded_experiment_manager_avec_ppo],
     tag="train/explained_variance",
     title="Training Explained Variance",
     show=False,
@@ -20,7 +32,7 @@ _ = plot_writer_data([default_ppo, avec_ppo],
 )
 
 
-_ = plot_writer_data([default_ppo, avec_ppo],
+_ = plot_writer_data([loaded_experiment_manager_default_ppo, loaded_experiment_manager_avec_ppo],
     tag="train/value_loss",
     title="Training Value Loss",
     show=False,
